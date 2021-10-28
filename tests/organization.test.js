7dd42cdfb8bb347539eb5cb40df39ca44af9860e
Aftab Alam Khan <aftab.khan@pager.com>
2021-10-28 10:58:26 -0400

    updated

--- tests/organization.test.js
+++ tests/organization.test.js
@@ -1,12 +1,31 @@
 const request = require('supertest')
 const app = require('../app')
-describe('Organization API', () => {
-    it('should create a new post', async (done) => {
+
+
+
+// test('login', () => {
+//     await request(app)
+//         .post('/organizations')
+//     .send({
+//         "name": "Juanito",
+//         "description": "Best Juanito",
+//         "url": "www.juanito.com",
+//         "code": "222",
+//         "type": "store2"
+//     })
+//         .set('Accept', 'application/json')
+//     .expect()
+    
+// });
+
+
+describe('Organization API', async() => {
+    it('should create a new post', async () => {
         const res = await request(app)
             .get('/')
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual("Organization API's");
-        done();
+        .expect(res.statusCode).toEqual(200)
+        .expect(res.text).toEqual("Organization API's")
+        .done();
     })
 
     // it('should create a new post', async () => {
